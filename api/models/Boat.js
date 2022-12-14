4aef6b60595ef6b0ca5051be55b57c8cb980d5e4
Karina Kniazeva <karishaknyazeva@gmail.com>
2022-12-14 18:19:02 +0100

    requerd felds

--- api/models/Boat.js
+++ api/models/Boat.js
@@ -1,7 +1,7 @@
 // api/models/Boat.js
 module.exports = {
     attributes: {
-        name: { type: 'string', columnType: 'varchar(80)', required: true },
+        name: { type: 'string', columnType: 'varchar(80)', required: true, unique: true },
         description: { type: 'string', columnType: 'varchar(80)' },
         price: { type: 'number',  columnType: 'DECIMAL (6,2)',  required: true},
         condition: { type: 'string', columnType: 'varchar(10)', required: true},
